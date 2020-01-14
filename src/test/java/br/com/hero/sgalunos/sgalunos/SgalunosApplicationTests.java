58b0e39fecca01dd962c712e84af6ed2118502c9
Matheus Lima <josematheuslima25@gmail.com>
2020-01-14 14:50:49 -0300

    First Commit

---
+++ src/test/java/br/com/hero/sgalunos/sgalunos/SgalunosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hero.sgalunos.sgalunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgalunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
